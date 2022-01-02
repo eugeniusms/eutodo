9736848b33901170ff8ab0571d22e2e6259c5997
eugeniusms <eugeniusms@gmail.com>
2022-01-02 17:30:11 +0700

    🔧 Linking Between Pages

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 const Navbar = () => {
     return (
         <nav>
             <div className="logo">
                 <h1>ToDo List</h1>
             </div>
-            <a>Home</a>
-            <a>About</a>
-            <a>ToDo Listing</a>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/todo"><a>ToDo Listing</a></Link>
         </nav>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Footer from '../comps/Footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
@@ -11,6 +12,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies nulla fermentum pellentesque tincidunt. Duis cursus mattis convallis.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies nulla fermentum pellentesque tincidunt. Duis cursus mattis convallis.</p>
+      <Link href="/todo">
+        <a>See Todolist</a>
+      </Link>
       <Footer />
     </div>
   )
