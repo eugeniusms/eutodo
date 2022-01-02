b74d39ee81c7c695db0e9e8633571e2936aee4ec
eugeniusms <eugeniusms@gmail.com>
2022-01-02 19:18:01 +0700

    🔨  Build Dynamic Routes Paths Todo Id

--- pages/todo/[id].js
+++ pages/todo/[id].js
@@ -1,3 +1,19 @@
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await res.json();
+
+    const paths = data.map(todo => {
+        return {
+            params: { id: todo.id.toString() }
+        }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
 const Details = () => {
     return (
         <div>
