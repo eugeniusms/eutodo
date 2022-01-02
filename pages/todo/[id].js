7dc2925f625f3fc8749a4e6b2bb279883be01ee7
eugeniusms <eugeniusms@gmail.com>
2022-01-02 19:28:25 +0700

    🔧 Fetching a Single Item

--- pages/todo/[id].js
+++ pages/todo/[id].js
@@ -14,10 +14,21 @@ export const getStaticPaths = async () => {
     }
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/' + id)
+    const data = await res.json();
+
+    return {
+        props: { todo: data}
+    }
+}
+
+const Details = ({ todo }) => {
     return (
         <div>
-            <h1>Details Page</h1>
+            <h1>{ todo.title }</h1>
+            <p>{ todo.completed.toString() }</p>
         </div>
     );
 }
