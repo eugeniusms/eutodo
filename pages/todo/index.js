fbfbbf72bc6ade4bcf2697ac3b2e26b7bd448bad
eugeniusms <eugeniusms@gmail.com>
2022-01-02 19:03:53 +0700

    🛠 Fetching Data (getStaticProps)

--- pages/todo/index.js
+++ pages/todo/index.js
@@ -1,7 +1,27 @@
-const Todo = () => {
+import styles from '../../styles/Todo.module.css'
+
+export const getStaticProps = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await res.json();
+
+    return {
+        props: { todos: data }
+    }
+
+}
+
+const Todo = ({ todos }) => {
     return (
         <div>
             <h1>All ToDo</h1>
+            {todos.map(todo => (
+                <div key={todo.id}>
+                    <a className={styles.single}>
+                        <h3>{ todo.title }</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 }
